573c9c5d7ec76bc36304de5208e5d055235152c2
owalid <othmane.el.ayadi@gmail.com>
2020-11-15 00:37:42 +0100

    first commit

---
+++ swifty_companion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swifty_companion
+//
+//  Created by owalid on 15/11/2020.
+//  Copyright © 2020 owalid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
